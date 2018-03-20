6371abb318a4e0c9e609cc9a0c44444a2ee7ab74
Redousa <36789450+Redousa@users.noreply.github.com>
2018-03-20 18:24:37 +0200

    .

--- bot.js
+++ bot.js
@@ -1673,7 +1673,53 @@ if (message.content.startsWith(".role ")) {
       });
 
 
+        client.on('message', message => {
+            if (message.content.startsWith(prefix + "uptime")) {
+                let uptime = client.uptime;
+            
+                let days = 0;
+                let hours = 0;
+                let minutes = 0;
+                let seconds = 0;
+                let notCompleted = true;
+            
+                while (notCompleted) {
+            
+                    if (uptime >= 8.64e+7) {
+            
+                        days++;
+                        uptime -= 8.64e+7;
+            
+                    } else if (uptime >= 3.6e+6) {
+            
+                        hours++;
+                        uptime -= 3.6e+6;
+            
+                    } else if (uptime >= 60000) {
+            
+                        minutes++;
+                        uptime -= 60000;
+            
+                    } else if (uptime >= 1000) {
+                        seconds++;
+                        uptime -= 1000;
+            
+                    }
+            
+                    if (uptime < 1000)  notCompleted = false;
+            
+                }
+               
 
+                const embed = new Discord.RichEmbed()
+                .setColor('RANDOM')
+                .addField("The bot have been working for : ", "`" + `${days} days, ${hours} hrs, ${minutes} min , ${seconds} sec` + "`")
+                .setFooter('requested by: ' + message.author.tag)
+                .setTimestamp()
+                message.channel.sendEmbed(embed)
+            
+            }
+            });
 
 
 
